0807cd62d8782773cecdc3b0cd138555d9666c1c
Wesley Reis <wesleydequeirozreis@gmail.com>
2021-07-09 15:32:51 -0300

    first commit

---
+++ src/main/java/com/talents/orange/marvelapi/MarvelapiApplication.java
@@ -0,0 +1,13 @@
+package com.talents.orange.marvelapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarvelapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarvelapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/talents/orange/marvelapi/MarvelapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.talents.orange.marvelapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarvelapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
